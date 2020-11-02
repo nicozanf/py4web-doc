537a19329ef632906048343b202ab0f299f67316
nico <nicozanf@gmail.com>
2020-11-02 22:17:41 +0100

    fix master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ release = '0.9 beta'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme',
+extensions = [
+		'sphinx_rtd_theme',
+	    	'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The master toctree document.
+master_doc = 'index'
+
