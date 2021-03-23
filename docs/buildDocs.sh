294b7e6f926debc6acbec37da53e1b4b2b8c863a
Nico Zanferrari <nicozanf@gmail.com>
2021-03-23 09:19:04 +0100

    added 'pip list'

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -20,7 +20,9 @@ apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-stemmer python3-git python3-pip python3-virtualenv python3-setuptools
   
 python3 -m pip install --upgrade rinohtype pygments sphinx-tabs
-  
+
+pip list
+
 #####################
 # DECLARE VARIABLES #
 #####################
