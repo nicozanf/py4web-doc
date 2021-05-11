adf24114232c6643b0e82301b756adbec1ccd6f8
Nico Zanferrari <nicozanf@gmail.com>
2021-05-11 17:39:46 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = 'images/logo.png'
+html_logo = 'images/logo_with_py4web.png'
+html_favicon = 'images/logo-32x32.ico'
 html_theme_options = {
     'logo_only': True,
     'display_version': True,
