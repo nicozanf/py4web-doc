21fb54bc31b4c070ba9e7115fa82bd7cb737ee49
Nico Zanferrari <nicozanf@gmail.com>
2024-04-06 09:51:32 +0200

    fix safe.directory
    add git config --global --add safe.directory /__w/py4web-doc/py4web-doc

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -28,6 +28,7 @@ python3 -m pip list
   
 pwd
 ls -lah
+git config --global --add safe.directory /__w/py4web-doc/py4web-doc
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
   
 # make a new temp dir which will be our GitHub Pages docroot
